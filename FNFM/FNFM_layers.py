b9d27447b1cb1a74f7e0e517d7cfebf903dfb3d5
lin920524-XIN <lin92052460@gmail.com>
2026-06-03 15:20:50 +0800

    Create FNFM_layers.py
    FNFM layers

---
+++ FNFM/FNFM_layers.py
@@ -0,0 +1,64 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+class FieldAwareFactorizationMachine(torch.nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.num_fields = len(field_dims)
+        self.embeddings = torch.nn.ModuleList([
+            torch.nn.Embedding(sum(field_dims), embed_dim) for _ in range(self.num_fields)
+        ])
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        for embedding in self.embeddings:
+            torch.nn.init.xavier_uniform_(embedding.weight.data)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        xs = [self.embeddings[i](x) for i in range(self.num_fields)]
+        ix = list()
+        for i in range(self.num_fields - 1):
+            for j in range(i + 1, self.num_fields):
+                ix.append(xs[j][:, i] * xs[i][:, j])
+        ix = torch.stack(ix, dim=1)
+        return ix
+
+class MultiLayerPerceptron(torch.nn.Module):
+
+    def __init__(self, input_dim, embed_dims, dropout, output_layer=True):
+        super().__init__()
+        layers = list()
+        for embed_dim in embed_dims:
+            layers.append(torch.nn.Linear(input_dim, embed_dim))
+            layers.append(torch.nn.BatchNorm1d(embed_dim))
+            layers.append(torch.nn.ReLU())
+            layers.append(torch.nn.Dropout(p=dropout))
+            input_dim = embed_dim
+        if output_layer:
+            layers.append(torch.nn.Linear(input_dim, 1))
+        self.mlp = torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        """
+        :param x: Float tensor of size ``(batch_size, embed_dim)``
+        """
+        return self.mlp(x)
+
+class FeaturesLinear(torch.nn.Module):
+
+    def __init__(self, field_dims, output_dim=1):
+        super().__init__()
+        self.fc = torch.nn.Embedding(sum(field_dims), output_dim)
+        self.bias = torch.nn.Parameter(torch.zeros((output_dim,)))
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return torch.sum(self.fc(x), dim=1) + self.bias
