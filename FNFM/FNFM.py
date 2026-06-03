5ff5aca80a484ac46a26f58a9eb3162c317a63d3
lin920524-XIN <lin92052460@gmail.com>
2026-06-03 16:02:28 +0800

    Update FNFM.py
    sort code

--- FNFM/FNFM.py
+++ FNFM/FNFM.py
@@ -1,16 +1,8 @@
 import torch
-
 from torchfm.layer import FieldAwareFactorizationMachine, MultiLayerPerceptron, FeaturesLinear
 
-
 class FieldAwareNeuralFactorizationMachineModel(torch.nn.Module):
-    """
-    A pytorch implementation of Field-aware Neural Factorization Machine.
-
-    Reference:
-        L Zhang, et al. Field-aware Neural Factorization Machine for Click-Through Rate Prediction, 2019.
-    """
-
+    
     def __init__(self, field_dims, embed_dim, mlp_dims, dropouts):
         super().__init__()
         self.linear = FeaturesLinear(field_dims)
@@ -21,9 +13,6 @@ class FieldAwareNeuralFactorizationMachineModel(torch.nn.Module):
         self.mlp = MultiLayerPerceptron(self.ffm_output_dim, mlp_dims, dropouts[1])
 
     def forward(self, x):
-        """
-        :param x: Long tensor of size ``(batch_size, num_fields)``
-        """
         cross_term = self.ffm(x).view(-1, self.ffm_output_dim)
         cross_term = self.bn(cross_term)
         cross_term = self.dropout(cross_term)
