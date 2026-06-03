5001fda0f8f4a5ced827a0f9abd8bdb170e2fc58
lin920524-XIN <lin92052460@gmail.com>
2026-06-03 14:58:11 +0800

    Create AFM_layers.py
    AFM layers

---
+++ AFM/AFM_layers.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+class FeaturesEmbedding(torch.nn.Module):
+
+    def __init__(self, field_dims, embed_dim):
+        super().__init__()
+        self.embedding = torch.nn.Embedding(sum(field_dims), embed_dim)
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+        torch.nn.init.xavier_uniform_(self.embedding.weight.data)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return self.embedding(x)
+
+class FeaturesLinear(torch.nn.Module):
+
+    def __init__(self, field_dims, output_dim=1):
+        super().__init__()
+        self.fc = torch.nn.Embedding(sum(field_dims), output_dim)
+        self.bias = torch.nn.Parameter(torch.zeros((output_dim,)))
+        self.offsets = np.array((0, *np.cumsum(field_dims)[:-1]), dtype=np.long)
+
+    def forward(self, x):
+        """
+        :param x: Long tensor of size ``(batch_size, num_fields)``
+        """
+        x = x + x.new_tensor(self.offsets).unsqueeze(0)
+        return torch.sum(self.fc(x), dim=1) + self.bias
+
+class AttentionalFactorizationMachine(torch.nn.Module):
+
+    def __init__(self, embed_dim, attn_size, dropouts):
+        super().__init__()
+        self.attention = torch.nn.Linear(embed_dim, attn_size)
+        self.projection = torch.nn.Linear(attn_size, 1)
+        self.fc = torch.nn.Linear(embed_dim, 1)
+        self.dropouts = dropouts
+
+    def forward(self, x):
+        """
+        :param x: Float tensor of size ``(batch_size, num_fields, embed_dim)``
+        """
+        num_fields = x.shape[1]
+        row, col = list(), list()
+        for i in range(num_fields - 1):
+            for j in range(i + 1, num_fields):
+                row.append(i), col.append(j)
+        p, q = x[:, row], x[:, col]
+        inner_product = p * q
+        attn_scores = F.relu(self.attention(inner_product))
+        attn_scores = F.softmax(self.projection(attn_scores), dim=1)
+        attn_scores = F.dropout(attn_scores, p=self.dropouts[0], training=self.training)
+        attn_output = torch.sum(attn_scores * inner_product, dim=1)
+        attn_output = F.dropout(attn_output, p=self.dropouts[1], training=self.training)
+        return self.fc(attn_output)
